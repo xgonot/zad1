9160d1f273f164a2d80b7d3af666475ee4443b29
Milan Mladoniczky <6153201+mladoniczky@users.noreply.github.com>
2022-05-11 01:33:14 +0200

    add reservation tests

--- src/test/java/sk/stuba/fei/uim/vsa/pr1/tests/ReservationTest.java
+++ src/test/java/sk/stuba/fei/uim/vsa/pr1/tests/ReservationTest.java
@@ -9,7 +9,6 @@ import sk.stuba.fei.uim.vsa.pr1.TestData;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
@@ -17,7 +16,6 @@ import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static sk.stuba.fei.uim.vsa.pr1.TestData.*;
 import static sk.stuba.fei.uim.vsa.pr1.TestUtils.*;
 
@@ -38,7 +36,7 @@ class ReservationTest {
     }
 
     @Test
-    void RESERVATION01_shouldCreateReservation() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    void RES01_shouldCreateReservation() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         Object carPark = carParkService.createCarPark("test8", "testtest", 12);
         assertNotNull(carPark);
         testShouldHaveId(carPark);
@@ -69,13 +67,13 @@ class ReservationTest {
                 assertNotNull(startTime);
                 assertTrue(new Date().after(startTime));
             } else {
-                log("Cannot test reservation for starting time. Field not found!");
+                throw new RuntimeException("Cannot test reservation for starting time. Field not found!");
             }
         }
     }
 
     @Test
-    void RESERVATION02_shouldCreateAndEndReservation() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, InterruptedException {
+    void RES02_shouldCreateAndEndReservation() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, InterruptedException {
         Object carPark = carParkService.createCarPark("test8", "testtest", 12);
         assertNotNull(carPark);
         testShouldHaveId(carPark);
@@ -111,7 +109,7 @@ class ReservationTest {
             if (dateFields.length > 0) {
                 assertTrue(Arrays.stream(dateFields).noneMatch(f -> isFieldNull(ended, f, Date.class)));
             } else {
-                log("Cannot test reservation for start time and end time. Field not found!");
+                throw new RuntimeException("Cannot test reservation for start time and end time. Field not found!");
             }
         }
 
@@ -139,13 +137,13 @@ class ReservationTest {
                     }
                 }));
             } else {
-                log("Cannot test reservation for price. Field not found!");
+                throw new RuntimeException("Cannot test reservation for price. Field not found!");
             }
         }
     }
 
     @Test
-    void RESERVATION03_shouldCreateAndGetReservations() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    void RES03_shouldCreateAndGetReservations() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         Object carPark = carParkService.createCarPark("test8", "testtest", 12);
         assertNotNull(carPark);
         testShouldHaveId(carPark);
@@ -172,12 +170,91 @@ class ReservationTest {
         assertEquals(getFieldValue(reservation, "id", Long.class), getFieldValue(reservations.get(0), "id", Long.class));
     }
 
-    void RESERVATION04_shouldCreateAndGetReservationByUser() {
+    @Test
+    void RES04_shouldCreateAndGetReservationByUser() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Object carPark = carParkService.createCarPark("test8", "testtest", 12);
+        assertNotNull(carPark);
+        testShouldHaveId(carPark);
+        Long carParkId = getFieldValue(carPark, "id", Long.class);
+        Object floor1 = carParkService.createCarParkFloor(carParkId, "Floor3-1");
+        assertNotNull(floor1);
+        Object spot1 = carParkService.createParkingSpot(carParkId, "Floor3-1", "1.01");
+        assertNotNull(spot1);
+        testShouldHaveId(spot1);
+        Long spot1Id = getFieldValue(spot1, "id", Long.class);
 
+        Object user = carParkService.createUser(TestData.User.firstName, TestData.User.lastName, TestData.User.email);
+        Object car = carParkService.createCar(getFieldValue(user, "id", Long.class),
+                TestData.Car.brand, TestData.Car.model, TestData.Car.colour, TestData.Car.ecv);
+        Long carId = getFieldValue(car, "id", Long.class);
+
+        Object reservation = carParkService.createReservation(spot1Id, carId);
+        assertNotNull(reservation);
+        testShouldHaveId(reservation);
+
+        List<Object> usersReservations = carParkService.getMyReservations(getFieldValue(user, "id", Long.class));
+        assertNotNull(usersReservations);
+        assertEquals(1, usersReservations.size());
+        assertEquals(getFieldValue(reservation, "id", Long.class), getFieldValue(usersReservations.get(0), "id", Long.class));
     }
 
-    void RESERVATION05_shouldCreateAndUpdateReservation() {
+    @Test
+    void RES05_shouldCreateAndUpdateReservation() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Object carPark = carParkService.createCarPark("test8", "testtest", 12);
+        assertNotNull(carPark);
+        testShouldHaveId(carPark);
+        Long carParkId = getFieldValue(carPark, "id", Long.class);
+        Object floor1 = carParkService.createCarParkFloor(carParkId, "Floor3-1");
+        assertNotNull(floor1);
+        Object spot1 = carParkService.createParkingSpot(carParkId, "Floor3-1", "1.01");
+        assertNotNull(spot1);
+        testShouldHaveId(spot1);
+        Long spot1Id = getFieldValue(spot1, "id", Long.class);
 
+        Object user = carParkService.createUser(TestData.User.firstName, TestData.User.lastName, TestData.User.email);
+        Object car = carParkService.createCar(getFieldValue(user, "id", Long.class),
+                TestData.Car.brand, TestData.Car.model, TestData.Car.colour, TestData.Car.ecv);
+        Long carId = getFieldValue(car, "id", Long.class);
+
+        Object reservation = carParkService.createReservation(spot1Id, carId);
+        assertNotNull(reservation);
+        testShouldHaveId(reservation);
+
+        String[] localDateFields = findFieldByType(reservation, LocalDateTime.class);
+        if (localDateFields.length > 0) {
+            LocalDateTime controlTime = LocalDateTime.now();
+            for (String localDateField : localDateFields) {
+                setFieldValue(reservation, localDateField, controlTime);
+            }
+            Object updatedRes = carParkService.updateReservation(reservation);
+            assertTrue(Arrays.stream(localDateFields).allMatch(f -> {
+                try {
+                    return Objects.equals(controlTime, getFieldValue(updatedRes, f, LocalDateTime.class));
+                } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }));
+        } else {
+            String[] dateFields = findFieldByType(reservation, Date.class);
+            if (dateFields.length > 0) {
+                Date controlTime2 = new Date();
+                for (String dateField : dateFields) {
+                    setFieldValue(reservation, dateField, controlTime2);
+                }
+                Object updatedRes = carParkService.updateReservation(reservation);
+                assertTrue(Arrays.stream(dateFields).allMatch(f -> {
+                    try {
+                        return Objects.equals(controlTime2, getFieldValue(updatedRes, f, Date.class));
+                    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                }));
+            } else {
+                throw new RuntimeException("Cannot test reservation for update. Field not found!");
+            }
+        }
     }
 
     private <T> T getStartDateField(Object reservation, Class<T> dateClass) {
